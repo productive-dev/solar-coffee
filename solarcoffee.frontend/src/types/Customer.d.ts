0db9ec62d9967499e80c6778224b9830b8484a28
wesdoyle <wesdoyle08@gmail.com>
2020-10-24 10:45:35 -0500

    Make params optional

--- solarcoffee.frontend/src/types/Customer.d.ts
+++ solarcoffee.frontend/src/types/Customer.d.ts
@@ -1,20 +1,20 @@
 export interface ICustomer {
-  id: number;
-  createdOn: Date;
+  id?: number;
+  createdOn?: Date;
   updatedOn?: Date;
-  firstName: string;
-  lastName: string;
-  primaryAddress: ICustomerAddress;
+  firstName?: string;
+  lastName?: string;
+  primaryAddress?: ICustomerAddress;
 }
 
 export interface ICustomerAddress {
-  id: number;
-  createdOn: Date;
+  id?: number;
+  createdOn?: Date;
   updatedOn?: Date;
-  addressLine1: string;
-  addressLine2: string;
-  city: string;
-  state: string;
-  postalCode: string;
-  country: string;
+  addressLine1?: string;
+  addressLine2?: string;
+  city?: string;
+  state?: string;
+  postalCode?: string;
+  country?: string;
 }
